de8b9923aa5e96bdf2d71e66783e97a64b870a14
vincelep <vincent.leparoux@gmail.com>
2019-01-30 23:25:11 +0100

    ajout des models + test :)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+require 'faker'
+
+Course.destroy_all
+Student.destroy_all
+
+10.times do 
+  course = Course.create!(name: Faker::Esport.game)
+end
+
+40.times do
+  student = Student.create!(name: Faker::Name.unique.name, course_id: (rand(Course.first.id..Course.last.id)))
+end 
